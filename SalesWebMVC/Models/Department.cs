43705fd790620645cecefe1aecec489f4d5f95c0
Venzke <gabrielc.moreira97@gmail.com>
2021-02-12 17:51:15 -0300

    Upload all files

---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } //Atributes
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department() //Construtor
+        {
+        }
+
+        public Department(int id, string name) //Construtor
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller) //Method to add seller in department.
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final) //Method to return total of the sales.
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+    }
+}
---
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -0,0 +1,34 @@
+﻿using SalesWebMVC.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyy}")]
+        public DateTime Date { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        public double Amount { get; set; } //Atributes
+        public SalesStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord() //Construtor
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, 
+            double amount, SalesStatus status, Seller seller) //Construtor
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
